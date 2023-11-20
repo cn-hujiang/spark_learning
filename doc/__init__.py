65dab9d28c5dc09beefddeb5174a55a0868d05ef
cn-hujiang <65960822+cn-hujiang@users.noreply.github.com>
2023-11-20 14:03:07 +0800

    Add files via upload

---
+++ doc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time : 2023/8/21
+# @Author : jiang.hu
+# @File : __init__.py.py 
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/module/lr.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time : 2023/8/22
+# @Author : jiang.hu
+# @File : lr.py 
+from sklearn.linear_model import LogisticRegression
+
+if __name__ == '__main__':
+    pass
